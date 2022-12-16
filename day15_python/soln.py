8469cf5e63d3c085a102f4ef52af05286f071336
Sumeet Agarwal <sumeet.a@gmail.com>
2022-12-15 20:18:45 -0800

    done day 15

--- day15_python/soln.py
+++ day15_python/soln.py
@@ -1,14 +1,9 @@
-if False:
-    FILENAME = 'sample.txt'
-    Y_TO_CHECK_PART1 = 10
-    MAX_COORD_VAL_PART2 = 20
-else:
-    FILENAME = 'input.txt'
-    Y_TO_CHECK_PART1 = 2000000
-    MAX_COORD_VAL_PART2 = 4_000_000
-
 from collections import namedtuple
 Point = namedtuple('Point', 'x y')
+
+FILENAME = 'input.txt'
+Y_TO_CHECK_PART1 = 2000000
+MAX_COORD_VAL_PART2 = 4_000_000
 
 def brange(lo, hi):
     lo = max(0, lo)
@@ -25,7 +20,6 @@ class Sensor(namedtuple('Sensor', 'point dist_to_beacon')):
     def excludes(self, other_point):
         dist = manhattan(self.point, other_point)
         return dist <= self.dist_to_beacon
-
     @property
     def points_at_edges(self):
         d = self.dist_to_beacon + 1
@@ -50,7 +44,6 @@ def all_excluded_xs(sensor, less_than_manhattan):
     lo_exclusive = abs(sensor.y - Y_TO_CHECK_PART1) - less_than_manhattan + sensor.x
     hi_exclusive = -abs(sensor.y - Y_TO_CHECK_PART1) + less_than_manhattan + sensor.x
     return set(range(lo_exclusive, hi_exclusive+1))
-
 
 excluded_poss = set()
 
@@ -77,16 +70,10 @@ for beacon in beacons:
             pass
 
 print('part 1:', len(excluded_poss))
-tuning_freq = lambda p: p.x * 4000000 + p.y
-
-def all_points():
-    for x in range(0, MAX_COORD_VAL_PART2):
-        for y in range(0, MAX_COORD_VAL_PART2):
-            yield Point(x, y)
 
 for sensor in sensors:
     for point in sensor.points_at_edges:
         if not any(sensor.excludes(point) for sensor in sensors):
-            print('part 2: ', tuning_freq(point))
+            tuning_freq = lambda p: p.x * 4000000 + p.y
+            print('part 2:', tuning_freq(point))
             exit(0)
-
