c7e9cc1635a59d8fa79d7f7618a23e07801ef8ea
Sumeet Agarwal <sumeet.a@gmail.com>
2022-12-15 19:16:54 -0800

    experiment with min max stuff doesn't seem to work

--- day15_python/soln.py
+++ day15_python/soln.py
@@ -15,6 +15,26 @@ class Sensor(namedtuple('Sensor', 'point dist_to_beacon')):
         dist = manhattan(self.point, other_point)
         return dist <= self.dist_to_beacon
 
+    @property
+    def min_x_seen(self):
+        return max(0,
+                self.point.x - self.dist_to_beacon)
+
+    @property
+    def max_x_seen(self):
+        return min(MAX_COORD_VAL_PART2,
+                self.point.x + self.dist_to_beacon)
+
+    @property
+    def min_y_seen(self):
+        return max(0,
+                self.point.y - self.dist_to_beacon)
+
+    @property
+    def max_y_seen(self):
+        return min(MAX_COORD_VAL_PART2,
+                self.point.y + self.dist_to_beacon)
+
 
 def manhattan(p1, p2):
     return abs(p1.x - p2.x) + abs(p1.y - p2.y)
@@ -68,5 +88,18 @@ for point in all_points():
         print(point)
         print(tuning_freq(point))
 
+max_min_x = float('inf')
+min_max_x = float('-inf')
+
+max_min_y = float('inf')
+min_max_y = float('-inf')
 for sensor in sensors:
-    print(sensor)
+    max_min_x = min(max_min_x, sensor.min_x_seen)
+    min_max_x = max(min_max_x, sensor.max_x_seen)
+    max_min_y = min(max_min_y, sensor.min_y_seen)
+    min_max_y = max(min_max_y, sensor.max_y_seen)
+
+print(f'max_min_x: {max_min_x}')
+print(f'min_max_x: {min_max_x}')
+print(f'max_min_y: {max_min_y}')
+print(f'min_max_y: {min_max_y}')
