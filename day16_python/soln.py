ee1c51e3986c5d625547c462492e72bc180691b6
Sumeet Agarwal <sumeet.a@gmail.com>
2022-12-18 02:08:30 -0800

    janky ass day16 soln thank u gupta

--- day16_python/soln.py
+++ day16_python/soln.py
@@ -22,27 +22,31 @@ for line in open('input.txt').read().splitlines():
     dests = list(map(valve_num, (w.strip(',') for w in words[9:])))
     valves[src] = Valve(rate, dests)
 
+def addv(open_valves, new):
+    return open_valves | (1 << new)
+
+def hasv(open_valves, v):
+    return open_valves & (1 << v)
+
 @cache
 def part1(src, time_remaining, open_valves):
     if not time_remaining: return 0
-
-    rate = sum(valves[valve].rate for valve in open_valves)
 
     this_valve = valves[src]
     nexts = []
     # 1. we could move to any of the dests, which takes 1 turn
     for dest in this_valve.dests:
-        next = rate + part1(dest, time_remaining-1, open_valves)
+        next = part1(dest, time_remaining-1, open_valves)
         nexts.append(next)
     # 2. we could open this valve, if rate > 0
-    if this_valve.rate > 0 and src not in open_valves:
-        next = rate + part1(src, time_remaining-1,
-                            tuple(sorted(set(open_valves+(src,)))))
+    if this_valve.rate > 0 and not hasv(open_valves, src):
+        rate = this_valve.rate * (time_remaining - 1)
+        next = rate + part1(src, time_remaining-1, addv(open_valves, src))
         nexts.append(next)
 
     return max(nexts)
 
-print('part 1:', part1(valve_num('AA'), 30, ()))
+print('part 1:', part1(valve_num('AA'), 30, 0))
 
 def customcache(part2_func):
     cache = {}
@@ -59,46 +63,53 @@ def customcache(part2_func):
 def part2(us_src, el_src, time_remaining, open_valves, us_parent, el_parent):
     if not time_remaining: return 0
 
-    rate = sum(valves[valve].rate for valve in open_valves)
-
     our_valve = valves[us_src]
     el_valve = valves[el_src]
     nexts = []
 
     # 2. we could open this valve, if rate > 0
-    if our_valve.rate > 0 and us_src not in open_valves:
+    if our_valve.rate > 0 and not hasv(open_valves, us_src):
+        our_rate = our_valve.rate * (time_remaining - 1)
+
         # 1. elph could move to any of the dests
         for eleph_dest in el_valve.dests:
             if el_parent == eleph_dest:
                 continue
+            # IDK ABOUT THIS
+            if hasv(open_valves,eleph_dest): continue
 
-            next = rate + part2(us_src, eleph_dest,
-                                time_remaining-1, 
-                                tuple(sorted(set((us_src,)+open_valves,
-                                    ))), us_src, el_src)
+            next = our_rate + part2(us_src, eleph_dest,
+                                    time_remaining-1, 
+                                    addv(open_valves, us_src),
+                                    us_src, el_src)
             nexts.append(next)
         # 2. eleph could open valve
-        if el_valve.rate > 0 and el_src not in open_valves and el_src != us_src:
-            next = rate + part2(us_src, el_src,
+        if el_valve.rate > 0 and not hasv(open_valves, el_src) and el_src != us_src:
+            el_rate = el_valve.rate * (time_remaining - 1)
+            next = our_rate+el_rate + part2(us_src, el_src,
                     time_remaining-1,
-                    tuple(sorted(set(open_valves+(el_src,us_src)))),
+                    addv(addv(open_valves, us_src), el_src),
                     us_src, el_src)
             nexts.append(next)
     else:
         # 1. we could move to any of the dests, which takes 1 turn
         for us_dest in our_valve.dests:
             if us_dest == us_parent: continue
+            # IDK ABOUT THIS
+            if hasv(open_valves, us_dest): continue
 
             # 1. elph could move to any of the dests
             for eleph_dest in el_valve.dests:
                 if eleph_dest == el_parent: continue
-                next = rate + part2(us_dest, eleph_dest,
-                                    time_remaining-1, open_valves, us_src, el_src)
+                if hasv(open_valves, eleph_dest): continue
+                next = part2(us_dest, eleph_dest,
+                             time_remaining-1, open_valves, us_src, el_src)
                 nexts.append(next)
             # 2. eleph could open valve
-            if el_valve.rate > 0 and el_src not in open_valves:
-                next = rate + part2(us_dest, el_src, time_remaining-1,
-                                    tuple(sorted(set(open_valves+(el_src,)))), us_src, el_src)
+            if el_valve.rate > 0 and not hasv(open_valves, el_src):
+                rate = el_valve.rate * (time_remaining - 1)
+                next = rate+part2(us_dest, el_src, time_remaining-1,
+                             addv(open_valves, el_src), us_src, el_src)
                 nexts.append(next)
 
 
@@ -106,4 +117,4 @@ def part2(us_src, el_src, time_remaining, open_valves, us_parent, el_parent):
         nexts = [0]
     return max(nexts)
 
-print('part 2:', part2(valve_num('AA'), valve_num('AA'), 26, (), None, None))
+print('part 2:', part2(valve_num('AA'), valve_num('AA'), 26, 0, None, None))
