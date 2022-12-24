f4670904e81761c013cf1d26ac4d783e2f66d199
Sumeet Agarwal <sumeet.a@gmail.com>
2022-12-23 21:58:01 -0500

    finish 18part1

--- day18_haskell/soln.hs
+++ day18_haskell/soln.hs
@@ -1,24 +1,28 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
+import Data.List (nub)
+import qualified Data.Map as Map
 import qualified Data.Set as Set
 
 data Point = Point Int Int Int deriving (Show, Eq, Ord)
 
 type Face = Set.Set Point
 
+type Counts = Map.Map Face Int
+
 splitOn :: Char -> String -> [String]
 splitOn _ [] = []
 splitOn c s = let (x, xs) = break (== c) s in x : splitOn c (drop 1 xs)
 
 faces :: Point -> [Face]
-faces (Point x y z) =
-  [ Set.fromList [Point x y z, Point (x + 1) y z, Point x (y + 1) z, Point x y (z + 1)],
-    Set.fromList [Point x y z, Point (x + 1) y z, Point (x + 1) (y + 1) z, Point x (y + 1) z],
-    Set.fromList [Point x y z, Point (x + 1) y z, Point (x + 1) y (z + 1), Point x y (z + 1)],
-    Set.fromList [Point x y z, Point x (y + 1) z, Point x (y + 1) (z + 1), Point x y (z + 1)],
-    Set.fromList [Point x y z, Point x (y + 1) z, Point (x + 1) (y + 1) z, Point (x + 1) y z],
-    Set.fromList [Point x y z, Point x y (z + 1), Point (x + 1) y (z + 1), Point (x + 1) y z]
-  ]
+faces (Point x y z) = [topFace, bottomFace, frontFace, backFace, leftFace, rightFace]
+  where
+    topFace = Set.fromList [Point x y (z + 1), Point (x + 1) y (z + 1), Point (x + 1) (y + 1) (z + 1), Point x (y + 1) (z + 1)]
+    bottomFace = Set.fromList [Point x y z, Point (x + 1) y z, Point (x + 1) (y + 1) z, Point x (y + 1) z]
+    frontFace = Set.fromList [Point x y z, Point x y (z + 1), Point (x + 1) y (z + 1), Point (x + 1) y z]
+    backFace = Set.fromList [Point x (y + 1) z, Point x (y + 1) (z + 1), Point (x + 1) (y + 1) (z + 1), Point (x + 1) (y + 1) z]
+    leftFace = Set.fromList [Point x y z, Point x y (z + 1), Point x (y + 1) (z + 1), Point x (y + 1) z]
+    rightFace = Set.fromList [Point (x + 1) y z, Point (x + 1) y (z + 1), Point (x + 1) (y + 1) (z + 1), Point (x + 1) (y + 1) z]
 
 choices :: [a] -> [(a, [a])]
 choices (x : xs) = (x, xs) : [(y, x : ys) | (y, ys) <- choices xs]
@@ -26,8 +30,10 @@ choices [] = []
 
 main :: IO ()
 main = do
-  s <- readFile "sample.txt"
+  s <- readFile "input.txt"
   let nums :: [[Int]] = map (map read . splitOn ',') $ lines s
   let cubes = map (\[x, y, z] -> Point x y z) nums
-  let f = length $ concat $ [filter (\face -> face `notElem` concatMap faces otherCubes) (faces thisCube) | (thisCube, otherCubes) <- choices cubes]
-  print f
+  let allFaces = concatMap faces cubes
+  let facesCounts = foldl (\acc face -> Map.insertWith (+) face 1 acc) Map.empty allFaces
+  putStr "part 1: "
+  print $ Map.size $ Map.filter (== 1) facesCounts
