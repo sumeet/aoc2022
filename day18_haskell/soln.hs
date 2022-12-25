46a159599b0008438a7ff3d51b1a610a9783ed50
Sumeet Agarwal <sumeet.a@gmail.com>
2022-12-24 22:11:00 -0500

    wip

--- day18_haskell/soln.hs
+++ day18_haskell/soln.hs
@@ -1,40 +1,65 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 import Data.Array (Array, array, (!), (//))
-import Data.List (nub)
+import Data.List (elemIndex, insert, nub)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Maybe (fromJust)
 import qualified Data.Set as Set
+import Debug.Trace (traceShowId)
 
 data Point = Point Int Int Int deriving (Show, Eq, Ord)
 
-data SparseRepr = SparseRepr { xy :: Map (Int, Int) (List Int)),
-                               xz :: Map (Int, Int) (List Int)),
-                               yz :: Map (Int, Int) (List Int))
-                             }
-type SparseCube = Array Int (Array Int (Array Int Bool))
+data SparseRepr = SparseRepr
+  { xy_z :: Map (Int, Int) [Int],
+    xz_y :: Map (Int, Int) [Int],
+    yz_x :: Map (Int, Int) [Int]
+  }
+  deriving (Show)
 
--- we actually need 3 sparse datastructures, one for each dimension
--- for example,
--- xy: [(x,y) => [z1->zlast]]
--- xz: [(x,z) => [y1->ylast]]
--- yz: [(y,z) => [x1->xlast]]
+----- (x,y) -> sorted [z1..2..zEnd]
+----- (x,z) -> sorted [y1..2..yEnd]
+----- (y,z) -> sorted [x1..5..xEnd]
 
-initSparse :: Int -> Int -> SparseCube
-initSparse min max = array (min, max) [(i, inner1) | i <- [min .. max]]
-  where
-    inner1 = array (min, max) [(i, inner2) | i <- [min .. max]]
-    inner2 = array (min, max) [(i, False) | i <- [min .. max]]
+pointTouchingOutside :: [Int] -> Int -> Bool
+pointTouchingOutside sorted n = case elemIndex n sorted of
+  (Just index) -> index == 0 || index == length sorted - 1
+  Nothing -> False
 
-addCube :: SparseCube -> Point -> SparseCube
-addCube cube (Point x y z) = cube // [(x, inner1 // [(y, inner2 // [(z, True)])])]
-  where
-    inner1 = cube ! x
-    inner2 = inner1 ! y
+faceTouchingOutside :: SparseRepr -> Point -> Bool
+faceTouchingOutside (SparseRepr xy_z xz_y yz_x) (Point x y z) =
+  let zs = Map.lookup (x, y) xy_z
+      ys = Map.lookup (x, z) xz_y
+      xs = Map.lookup (y, z) yz_x
+   in case (xs, ys, zs) of
+        (Just xs, Just ys, Just zs) -> pointTouchingOutside xs x || pointTouchingOutside ys y || pointTouchingOutside zs z
+        _ -> False
+
+initSparse :: SparseRepr
+initSparse = SparseRepr Map.empty Map.empty Map.empty
+
+insertMany :: [Int] -> [Int] -> [Int]
+insertMany sorted ns = nub $ foldl (flip insert) sorted ns
+
+addCube :: SparseRepr -> Point -> SparseRepr
+addCube (SparseRepr xy_z xz_y yz_x) (Point x y z) =
+  SparseRepr
+    (Map.insertWith insertMany (x, y) [z] xy_z)
+    (Map.insertWith insertMany (x, z) [y] xz_y)
+    (Map.insertWith insertMany (y, z) [x] yz_x)
 
 splitOn :: Char -> String -> [String]
 splitOn _ [] = []
 splitOn c s = let (x, xs) = break (== c) s in x : splitOn c (drop 1 xs)
+
+searchTrapped :: Point -> Set.Set Point -> Set.Set Point -> Set.Set Point
+searchTrapped pt visited allFaces
+  | pt `Set.member` visited = Set.empty
+  | otherwise =
+    let nexts = faces pt
+     in if Set.fromList nexts `Set.isSubsetOf` Set.union visited allFaces
+          then Set.singleton pt
+          else foldl Set.union Set.empty $ map (\p -> searchTrapped p (Set.insert pt visited) allFaces) nexts
 
 main :: IO ()
 main = do
@@ -45,19 +70,25 @@ main = do
   -- part1, not sure why. but it's removing shit tons of code. see part1_old.hs for my original soln
   putStr "part 1: "
   let cubeSet = Set.fromList cubes
-  let maybeTrappedCubes = filter (`Set.notMember` cubeSet) $ concatMap trappers cubes
-  print $ length maybeTrappedCubes
+  let allFaces = filter (`Set.notMember` cubeSet) $ concatMap faces cubes
+  print $ length allFaces
 
---   putStr "part 2: "
---   let min = minimum $ concat nums
---   let max = maximum $ concat nums
---   print $ cubes
---   let sparseCube = foldl addCube (initSparse min max) cubes
---   print sparseCube
---   pure ()
+  putStr "part 2: "
+  let visited :: Set.Set Point = Set.fromList []
+  let trapped :: Set.Set Point = Set.fromList []
+  let allFacesSet = Set.fromList allFaces
+  print $ foldl Set.union Set.empty $ map (\p -> searchTrapped p visited allFacesSet) (Set.toList allFacesSet)
+  --let startingPoint = minimum allFaces
+  pure ()
 
-trappers :: Point -> [Point]
-trappers (Point x y z) =
+--   let sparse = foldl addCube initSparse allFaces
+--   let touchingOutside = Set.fromList $ filter (faceTouchingOutside sparse) allFaces
+--   print $ length $ Set.intersection touchingOutside (Set.fromList $ faces (Point 2 2 5))
+
+--print $ faceTouchingOutside sparse (Point 2 2 5)
+
+faces :: Point -> [Point]
+faces (Point x y z) =
   [ Point (x + 1) y z,
     Point (x - 1) y z,
     Point x (y -1) z,
