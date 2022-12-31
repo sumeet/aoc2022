9ad2d08c39c1a4e6459372eddb2b92d8f444f449
Sumeet Agarwal <sumeet.a@gmail.com>
2022-12-31 00:28:05 -0800

    wip

--- day22_rust/src/main.rs
+++ day22_rust/src/main.rs
@@ -1,25 +1,43 @@
 use std::iter::from_fn;
 
 struct Transition {
+    name: String,
     src: (Vec<Point>, Dir),
     dst: (Vec<Point>, Dir),
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 struct Point {
     x: usize,
     y: usize,
 }
 
+impl Point {
+    fn apply(mut self, dir: Dir) -> Self {
+        match dir {
+            UP => self.y -= 1,
+            DOWN => self.y += 1,
+            LEFT => self.x -= 1,
+            RIGHT => self.x += 1,
+            _ => unimplemented!(),
+        }
+        self
+    }
+}
+
 impl Transition {
-    fn new(src: (Vec<Point>, Dir), dst: (Vec<Point>, Dir)) -> Self {
-        Self { src, dst }
+    fn new(src: (Vec<Point>, Dir), dst: (Vec<Point>, Dir), name: String) -> Self {
+        Self { src, dst, name }
     }
 
     fn rev(&self) -> Self {
         let src = (self.dst.0.clone(), self.dst.1.flip());
         let dst = (self.src.0.clone(), self.src.1.flip());
-        Self { src, dst }
+        Self {
+            src,
+            dst,
+            name: format!("{}-rev", self.name),
+        }
     }
 }
 // ([(99,0)..(99, 49)],
@@ -37,61 +55,73 @@ fn transitions() -> Vec<Transition> {
         Transition::new(
             (range((99, 0), (99, 49)), RIGHT),
             (range((100, 0), (100, 49)), RIGHT),
+            "1 -> 2".into(),
         ),
         // 1 -> 3 (bottom of 1 -> top of 3)
         Transition::new(
             (range((50, 49), (99, 49)), DOWN),
             (range((50, 50), (99, 50)), DOWN),
+            "1 -> 3".into(),
         ),
         // 1 -> 5 (left of 1 -> left of 5)
         Transition::new(
             (range((50, 0), (50, 49)), LEFT),
             (range((0, 149), (0, 100)), RIGHT),
+            "1 -> 5".into(),
         ),
         // 1 -> 6 (top of 1, left of 6)
         Transition::new(
             (range((50, 0), (99, 0)), UP),
-            (range((0, 150), (49, 199)), RIGHT),
+            (range((0, 150), (0, 199)), RIGHT),
+            "1 -> 6".into(),
         ),
         // 2 -> 3 (bottom of 2 -> right of 3)
         Transition::new(
             (range((100, 49), (149, 49)), DOWN),
-            (range((99, 50), (99, 99)), RIGHT),
+            (range((99, 50), (99, 99)), LEFT),
+            "2 -> 3".into(),
         ),
         // 2 -> 4 (right of 2 -> right of 4)
         Transition::new(
             (range((149, 49), (149, 0)), RIGHT),
             (range((99, 100), (99, 149)), LEFT),
+            "2 -> 4".into(),
         ),
         // 2 -> 6 (top of 2 -> bottom of 6)
         Transition::new(
             (range((100, 0), (149, 0)), UP),
             (range((0, 199), (49, 199)), UP),
+            "2 -> 6".into(),
         ),
         // 3 -> 4 (bottom of 3 -> top of 4)
         Transition::new(
             (range((50, 99), (99, 99)), DOWN),
             (range((50, 100), (99, 100)), DOWN),
+            "3 -> 4".into(),
         ),
         // 3 -> 5 (left of 3 -> top of 5)
         Transition::new(
             (range((50, 50), (50, 99)), LEFT),
             (range((0, 100), (0, 149)), DOWN),
+            "3 -> 5".into(),
         ),
         // 4 -> 5 (left of 4, right of 5)
         Transition::new(
             (range((50, 100), (50, 149)), LEFT),
             (range((49, 100), (49, 149)), LEFT),
+            "4 -> 5".into(),
         ),
         // 4 -> 6 (bottom of 4 -> right of 6)
         Transition::new(
             (range((50, 149), (99, 149)), DOWN),
             (range((49, 150), (49, 199)), LEFT),
+            "4 -> 6".into(),
         ),
         // 5 -> 6 (bottom of 5, top of 6)
         Transition::new(
             (range((0, 149), (49, 149)), DOWN),
             (range((0, 150), (49, 150)), DOWN),
+            "5 -> 6".into(),
         ),
     ]
     .into_iter()
@@ -103,6 +133,37 @@ impl From<(usize, usize)> for Point {
     fn from((x, y): (usize, usize)) -> Self {
         Self { x, y }
     }
+}
+
+fn search_transitions(
+    point: impl Into<Point>,
+    dir: Dir,
+    transitions: &[Transition],
+) -> Option<(Point, Dir)> {
+    let point = point.into();
+    for tran in transitions {
+        if dir != tran.src.1 {
+            continue;
+        }
+        for (i, trans_pt) in tran.src.0.iter().enumerate() {
+            if point == *trans_pt {
+                println!("found transition {}", tran.name);
+                return Some((tran.dst.0[i], tran.dst.1));
+            }
+        }
+    }
+    None
+    // let src_transition = transitions
+    //     .iter()
+    //     .find(|t| t.src.0.contains(&point) && t.src.1 == dir)?;
+    // let i = src_transition
+    //     .src
+    //     .0
+    //     .iter()
+    //     .position(|p| *p == point)
+    //     .unwrap();
+    // let dst_point = src_transition.dst.0[i];
+    // Some((dst_point, src_transition.dst.1))
 }
 
 type Range = Vec<Point>;
@@ -174,9 +235,23 @@ fn print_grid(g: &Vec<Vec<char>>, cur: Point) {
     }
 }
 
+fn next_pos(cur: Point, dir: Dir, transitions: &[Transition]) -> (Point, Dir) {
+    if let Some(trans) = search_transitions(cur, dir, transitions) {
+        println!(
+            "got edge on: {:?} -> {:?}",
+            (cur.x, cur.y),
+            (trans.0.x, trans.0.y)
+        );
+        trans
+    } else {
+        println!("not edge");
+        (cur.apply(dir), dir)
+    }
+}
+
 fn main() {
     let transitions = transitions();
-    assert_eq!(transitions.len(), 24);
+    // assert_eq!(transitions.len(), 24);
 
     let (gridlines, instrs) = INPUT.trim_end().split_once("\n\n").unwrap();
     let mut g: Vec<Vec<char>> = gridlines
@@ -195,112 +270,28 @@ fn main() {
     for inst in Inst::iter_from(instrs) {
         match inst {
             Inst::Move(mut n) => {
-                // println!("Move {} {}", n, facing.to_string());
-                match facing {
-                    UP => {
-                        let col = grid.col(pos.x);
-                        let mut col_cycle = col
-                            .iter()
-                            .enumerate()
-                            .rev()
-                            .cycle()
-                            .skip(col.len() - pos.y)
-                            .filter(|(_, &c)| c != ' ')
-                            .peekable();
+                println!("Moving {} {}", n, facing.to_string());
+                for _ in 0..n {
+                    let (next_pos, next_dir) = next_pos(pos, facing, &transitions);
+                    if grid.row(next_pos.y)[next_pos.x] == '.' {
+                        g[pos.y][pos.x] = match facing {
+                            UP => '^',
+                            DOWN => 'v',
+                            RIGHT => '>',
+                            LEFT => '<',
+                            _ => unreachable!(),
+                        };
 
-                        let mut prev_y = pos.y;
-                        while n > 0 {
-                            let (y, &char) = col_cycle.next().unwrap();
-                            if char == '.' {
-                                g[prev_y][pos.x] = '^';
-                                prev_y = y;
-                                n -= 1;
-                                continue;
-                            } else if char == '#' {
-                                break;
-                            }
-                        }
-                        pos.y = prev_y;
+                        pos = next_pos;
+                        facing = next_dir;
                     }
-                    LEFT => {
-                        let row = grid.row(pos.y);
-                        let mut row_cycle = row
-                            .iter()
-                            .enumerate()
-                            .rev()
-                            .cycle()
-                            .skip(row.len() - pos.x)
-                            .filter(|(_, &c)| c != ' ');
-                        let mut prev_x = pos.x;
-                        while n > 0 {
-                            let (x, &char) = row_cycle.next().unwrap();
-                            if char == '.' {
-                                g[pos.y][prev_x] = '<';
-                                prev_x = x;
-                                n -= 1;
-                                continue;
-                            } else if char == '#' {
-                                break;
-                            }
-                        }
-                        pos.x = prev_x;
-                    }
-                    DOWN => {
-                        let col = grid.col(pos.x);
-                        let mut col_cycle = col
-                            .iter()
-                            .enumerate()
-                            .cycle()
-                            .skip(pos.y + 1)
-                            .filter(|(_, &c)| c != ' ');
-                        let mut prev_y = pos.y;
-                        'inner: while n > 0 {
-                            let (y, &char) = col_cycle.next().unwrap();
-                            if char == '.' {
-                                g[prev_y][pos.x] = 'v';
-                                prev_y = y;
-                                n -= 1;
-                                continue 'inner;
-                            } else if char == '#' {
-                                break;
-                            }
-                        }
-                        pos.y = prev_y;
-                    }
-                    RIGHT => {
-                        let row = grid.row(pos.y);
-                        let mut row_cycle = row
-                            .iter()
-                            .enumerate()
-                            .cycle()
-                            .skip(pos.x + 1)
-                            .filter(|(_, &c)| c != ' ');
-                        let mut prev_x = pos.x;
-                        while n > 0 {
-                            let (x, &char) = row_cycle.next().unwrap();
-                            if char == ' ' {
-                                g[pos.y][prev_x] = '>';
-                                prev_x = x;
-                                continue;
-                            }
-                            if char == '.' {
-                                g[pos.y][prev_x] = '>';
-                                prev_x = x;
-                                n -= 1;
-                                continue;
-                            }
-                            if char == '#' {
-                                break;
-                            }
-                        }
-                        pos.x = prev_x;
-                    }
-                    _ => unreachable!(),
+                    println!("next: {:?}", (pos.x, pos.y));
                 }
-                // print_grid(&g, pos);
-                // println!();
-                // println!();
-                // println!();
+
+                print_grid(&g, pos);
+                println!();
+                println!();
+                println!();
             }
             Inst::Turn(turn) => match turn {
                 'L' => facing = facing.turn_left(),
